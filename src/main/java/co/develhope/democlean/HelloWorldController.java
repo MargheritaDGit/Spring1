fcdb765bd90d59383d8a69f95aabe06c62143ed8
Margherita <margherita.deodato14@gmail.com>
2024-02-02 09:54:16 +0100

    es 1 corretto

--- src/main/java/co/develhope/democlean/HelloWorldController.java
+++ src/main/java/co/develhope/democlean/HelloWorldController.java
@@ -4,16 +4,18 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
-@RequestMapping("/V1")
+
+
 @RestController
+@RequestMapping("/v1")
 public class HelloWorldController {
-    @GetMapping("/HelloWorld")
-    public String HelloWorld() {
-        return "HelloWorld";
-    }
-    @GetMapping ("/Ciao")
-    public String ciao (@RequestParam String nome, @RequestParam String provincia) {
-        return "Ciao come è il tempo in provincia " + nome + " provincia ";
+    // @GetMapping("/HelloWorld")
+    // public String HelloWorld() {
+    //    return "HelloWorld";
+    // }
+    @GetMapping("/Ciao")
+    public String ciao(@RequestParam String nome, @RequestParam String provincia) {
+        return "Ciao " + nome + ",  come è il tempo in " + provincia;
     }
 }
 
